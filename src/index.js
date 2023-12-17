fdbf5e5cc18ece217a5326cc900735215584d869
Hebert S. Pereira <hebertsp.x@gmail.com>
2023-12-17 17:51:03 -0300

    fix error deploy vercel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 var express = require('express');
-const { userRouter } = require('./src/module/users/users.controller');
+const { userRouter } = require('./module/users/users.controller');
 var app = express();
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRouter)
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Serviço liberado na porta 3000!');
 });
