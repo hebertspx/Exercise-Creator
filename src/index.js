2ef1ea89dbe8d8ec1c47144937f00891c2855aa9
Hebert S. Pereira <hebertsp.x@gmail.com>
2023-12-17 18:10:04 -0300

    Presentation Page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', function (req, res) {
+    res.json({ data: "Servidor Online!" })
+})
+
 app.use('/users', userRouter)
 
 app.listen(3000, function () {
