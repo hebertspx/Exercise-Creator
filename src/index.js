5d559e5c70549cdb1e7e34a716ee99056f4acd09
Hebert S. Pereira <hebertsp.x@gmail.com>
2023-12-17 18:51:10 -0300

    Implementing CORS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 var express = require('express');
 const { userRouter } = require('./module/users/users.controller');
 var app = express();
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
+app.get('/', function (_req, res) {
     res.json({ data: "Servidor Online!" })
 })
 
